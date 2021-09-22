0b577e24a7abf0a66b046b8642816bed40dcd048
Jason Lee <getbest@gmail.com>
2021-09-22 17:12:53 -0400

    first load to git repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import marked from "marked";
+import { useState, useEffect } from "react";
+
+const Preview = (props) => {
+  return (
+    <div
+      id="preview"
+      dangerouslySetInnerHTML={{ __html: props.renderHtml }}
+    ></div>
+  );
+};
 
 function App() {
+  const defVal =
+    "# Markdown Previewer\n" +
+    "## Default value\n" +
+    "[link](www.google.ca)\n" +
+    "`code sample`\n" +
+    "```\n" +
+    "function aaa (arg)  { return arg + 1}\n" +
+    "aaa()\n" +
+    "```\n" +
+    "* list1\n" +
+    "* list2\n" +
+    "* list3\n" +
+    " > This is a very long line that will still be quoted properly when it wraps. Oh boy let's keep writing to make sure this is long enough to actually wrap for everyone. \n\n" +
+    "![logo](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png)\n\n" +
+    "__Bolded text__";
+  const [inputText, setInputText] = useState(defVal);
+  const [previewText, setPreviewText] = useState("");
+
+  const fireChange = (e) => {
+    setInputText(e.target.value);
+  };
+  useEffect(() => {
+    setPreviewText(marked(inputText, { breaks: true }));
+  }, [inputText]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Input Markdown</h2>
+      <textarea
+        id="editor"
+        onChange={fireChange}
+        defaultValue={defVal}
+      ></textarea>
+      <h2>HTML presentation</h2>  
+      <Preview renderHtml={previewText} />
     </div>
   );
 }
